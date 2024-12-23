4e3ee6070d22c423c22986546dc2ce870ec1db60
NikitaKuzire <nikitakuzire@gmail.com>
2024-12-23 20:53:03 +0300

    hello

---
+++ script.js
@@ -0,0 +1,30 @@
+let zina = document.querySelector(".zina")
+
+    for(let i = 0; i < zina.length; i++){
+        zina[i].addEventListener('click', function(){
+            if(i == 0){
+              let every= one + every
+            }
+            else if (i == 1){
+                let every= two + every 
+            }
+            else if (i == 2){
+                let every= three + every 
+            }
+            else if (i == 3){
+                let every= four + every 
+            }
+            else if (i == 5){
+                let every= five + every 
+            }
+            console.log(every)
+        })}
+        let one = 1499
+        let every = 0
+        let two = 869
+        let three = 1299
+        let four = 1399
+        let five = 700
+        
+        let price = [1499, 869, 1199, 1299, 1399, 700 ] 
+    
